490fbb92671caf91dadab58c400c28f4513688f8
Abdennasser Bentaleb <abdennasserbentaleb@gmail.com>
2025-09-05 10:01:47 +0100

    chore: initialize spring boot 3.5.11 webhook aggregator skeleton

---
+++ src/main/java/com/resilient/webhook/WebhookAggregatorApplication.java
@@ -0,0 +1,12 @@
+package com.resilient.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookAggregatorApplication.class, args);
+    }
+}
